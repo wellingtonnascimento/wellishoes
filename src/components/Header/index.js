1bcac6cb124bca93bc9c056080870d63d65d9dfd
Wellington N <wellingto_07@hotmail.com>
2020-09-06 17:20:15 -0300

    header cart

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import { MdShoppingBasket } from 'react-icons/md';
 
@@ -7,22 +8,22 @@ import { Container, Cart } from './stylesHeader';
 
 import logo from '../../assets/images/logoffff.png';
 
-function Header() {
+export default function Header() {
+  const cartSize = useSelector((state) => state.cart.length);
+
   return (
     <Container>
       <Link to="/">
-        <img src={logo} alt="Logo" width="150" />
+        <img src={logo} alt="Rocketshoes" />
       </Link>
 
       <Cart to="/cart">
         <div>
-          <strong>Meu Carrinho</strong>
-          <span>3 itens</span>
+          <strong>Meu carrinho</strong>
+          <span>{cartSize} itens</span>
         </div>
-        <MdShoppingBasket size={36} color="#fff" />
+        <MdShoppingBasket size={36} color="#FFF" />
       </Cart>
     </Container>
   );
 }
-
-export default Header;
