8a81d7dd2103c7d949e5321f231541fd7581e909
Wellington N <wellingto_07@hotmail.com>
2020-09-09 01:00:32 -0300

    header size logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -11,16 +11,26 @@ import logo from '../../assets/images/logoffff.png';
 export default function Header() {
   const cartSize = useSelector((state) => state.cart.length);
 
+  function showCartSize() {
+    if (!cartSize) {
+      return 'Nenhum item';
+    }
+    if (cartSize === 1) {
+      return `${cartSize} item`;
+    }
+    return `${cartSize} itens`;
+  }
+
   return (
     <Container>
       <Link to="/">
-        <img src={logo} alt="Rocketshoes" height="250" />
+        <img src={logo} alt="Rocketshoes" height="200" />
       </Link>
 
       <Cart to="/cart">
         <div>
           <strong>Meu carrinho</strong>
-          <span>{cartSize} itens</span>
+          <span>{showCartSize()}</span>
         </div>
         <MdShoppingBasket size={36} color="#FFF" />
       </Cart>
