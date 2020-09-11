ccde05f6dcca2e8ad3d5bab449580ff58a81eed4
Wellington N <wellingto_07@hotmail.com>
2020-09-11 01:39:54 -0300

    dropdown  cart

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,19 +1,34 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 import { MdShoppingBasket } from 'react-icons/md';
 
-import { Container, Cart } from './stylesHeader';
+import { formatPrice } from '../../util/format';
+import { Container, Cart, Dropdown } from './stylesHeader';
 
 import logo from '../../assets/images/logoffff.png';
 
 export default function Header() {
-  const cartSize = useSelector((state) => state.cart.length);
+  const location = useLocation();
 
+  const { cart, total, cartSize } = useSelector((state) => ({
+    cartSize: state.cart.length,
+    cart: state.cart.slice(0, 3).map((product) => ({
+      ...product,
+      price: formatPrice(product.price),
+    })),
+    total: formatPrice(
+      state.cart.reduce((total, product) => {
+        return total + product.price * product.amount;
+      }, 0),
+    ),
+  }));
+
+  console.log(cart);
   function showCartSize() {
     if (!cartSize) {
-      return 'Nenhum item';
+      return '';
     }
     if (cartSize === 1) {
       return `${cartSize} item`;
@@ -24,15 +39,36 @@ export default function Header() {
   return (
     <Container>
       <Link to="/">
-        <img src={logo} alt="Rocketshoes" height="200" />
+        <img src={logo} alt="WelliShoes" height="200" />
       </Link>
 
       <Cart to="/cart">
+        <strong>Meu carrinho</strong>
         <div>
-          <strong>Meu carrinho</strong>
+          <MdShoppingBasket size={36} color="#FFF" />
           <span>{showCartSize()}</span>
         </div>
-        <MdShoppingBasket size={36} color="#FFF" />
+
+        {!!cart.length && location.pathname !== '/cart' && (
+          <Dropdown id="ca">
+            {cart.map((product) => (
+              <div>
+                <img src={product.image} alt={product.title} />
+                <div className="title">
+                  {product.title}
+                  <p>
+                    {product.amount} x <span>{product.price}</span>
+                  </p>
+                </div>
+              </div>
+            ))}
+            {cartSize > 3 && <div className="more">...</div>}
+            <h2>
+              <span>TOTAL:</span>
+              <span>{total}</span>
+            </h2>
+          </Dropdown>
+        )}
       </Cart>
     </Container>
   );
--- src/components/Header/stylesHeader.js
+++ src/components/Header/stylesHeader.js
@@ -12,23 +12,122 @@ export const Cart = styled(Link)`
   display: flex;
   align-items: center;
   text-decoration: none;
-  transition: opacity 0.2;
+  position: relative;
 
-  &:hover {
+  &:hover strong {
     opacity: 0.7;
   }
 
-  div {
-    text-align: right;
-    margin-right: 10px;
+  &:hover div:last-child {
+    opacity: 1;
+    visibility: visible;
+    transform: none;
+  }
 
-    strong {
+  strong {
+    display: block;
+    color: #fff;
+    text-transform: uppercase;
+    margin-right: 5px;
+    transition: opacity 0.2s;
+  }
+
+  div {
+    position: relative;
+    > span {
       display: block;
-      color: #fff;
-    }
-    span {
+      position: absolute;
+      width: 20px;
+      height: 20px;
+      background: #4682b4;
+      right: -4px;
+      top: -8px;
       font-size: 12px;
+      line-height: 21px;
+      overflow: hidden;
       color: #fff;
+      font-weight: bold;
+      border-radius: 100%;
+      text-align: center;
+    }
+  }
+`;
+
+export const Dropdown = styled.div`
+  position: absolute !important;
+  top: 100%;
+  right: 0;
+  background: #4682b4;
+  box-shadow: 0 3px 5px rgba(0, 0, 0, 0.3);
+  z-index: 5;
+  width: 220px;
+  border-radius: 4px;
+  padding: 20px;
+  opacity: 0;
+  visibility: hidden;
+  transition: all 0.3s;
+  transform: translateY(10px);
+
+  > div {
+    display: flex;
+
+    + div {
+      margin-top: 15px;
+    }
+
+    img {
+      border-radius: 3px;
+      width: 50px;
+      padding: 5px;
+      align-self: flex-start;
+      background: #fff;
+    }
+
+    div {
+      color: #fff;
+      font-size: 14px;
+      margin-left: 10px;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+
+      p {
+        font-size: 11px;
+        margin-top: 3px;
+
+        span {
+          font-weight: bold;
+          font-size: 13px;
+        }
+      }
+    }
+  }
+
+  .more {
+    width: 100%;
+    margin: 5px 0;
+    justify-content: center;
+    color: rgba(255, 255, 255, 0.8);
+    letter-spacing: 3px;
+    font-size: 15px;
+  }
+
+  h2 {
+    border-top: 1px solid rgba(0, 0, 0, 0.15);
+    margin-top: 20px;
+    padding-top: 20px;
+    display: flex;
+    justify-content: space-between;
+    align-items: baseline;
+
+    span:first-child {
+      color: rgba(255, 255, 255, 0.8);
+      font-size: 12px;
+    }
+    span:last-child {
+      color: #fff;
+      font-size: 22px;
+      font-weight: bold;
     }
   }
 `;
