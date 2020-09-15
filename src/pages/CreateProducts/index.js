31ad414473ced55a3d1e1438999b905e87431567
Wellington N <wellingto_07@hotmail.com>
2020-09-15 08:38:51 -0300

    create products

--- src/pages/CreateProducts/index.js
+++ src/pages/CreateProducts/index.js
@@ -1,28 +1,79 @@
-import React from 'react';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
 import { Container, Form } from './styledCreateProducts';
+import api from '../../services/api';
+
+import Loading from '../../components/Loading';
 
 export default function CreateProducts() {
+  const imageInputRef = useRef(null);
+  const [imageBase64, setImageBase64] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    function onImageChange(event) {
+      const reader = new FileReader();
+
+      const file = event.target.files[0];
+
+      reader.onload = function () {
+        setImageBase64(reader.result);
+      };
+
+      if (file) {
+        reader.readAsDataURL(file);
+      }
+    }
+
+    imageInputRef.current.addEventListener &&
+      imageInputRef.current.addEventListener('change', onImageChange);
+  }, [setImageBase64]);
+
+  const handleSubmit = useCallback(
+    async (e) => {
+      if (isLoading) return;
+      e.persist();
+      e.preventDefault();
+      setIsLoading(true);
+      const fields = [...e.target.elements].reduce((fields, input) => {
+        if (input.type === 'submit') {
+          return fields;
+        }
+
+        return { ...fields, [input.name]: input.value };
+      }, {});
+
+      const { data } = await api.post('products', {
+        ...fields,
+        image: imageBase64,
+      });
+      setIsLoading(false);
+      debugger;
+      e.target.reset();
+    },
+    [setIsLoading],
+  );
+
   return (
     <Container>
-      <Form>
-        <label for="img">Imagem Produto</label>
-        <input id="img"></input>
+      <Form onSubmit={handleSubmit}>
+        <div className="imageBlock">
+          <img src={imageBase64} />
+        </div>
+        <label htmlFor="img">Imagem Produto</label>
+        <input type="file" id="image" name="image" ref={imageInputRef} />
 
-        <label for="title">Titulo</label>
-        <input type="text" id="title" name="title" placeholder="Titulo.." />
+        <label htmlFor="title">Titulo</label>
+        <input type="text" placeholder="Titulo.." name="title" />
 
-        <label for="value">Valor</label>
-        <input type="text" id="value" name="value" placeholder="Valor" />
+        <label htmlFor="value">Valor</label>
+        <input type="text" placeholder="Valor" name="price" />
 
-        <label for="quantity">Quantidade</label>
-        <input
-          type="text"
-          id="quantity"
-          name="quantity"
-          placeholder="Quantidade"
-        />
+        <label htmlFor="quantity">Quantidade</label>
+        <input type="text" placeholder="Quantidade" name="quantity" />
 
-        <input type="submit" value="Submit" />
+        <button type="submit" value="Submit">
+          {isLoading ? <Loading /> : 'Salvar'}
+        </button>
       </Form>
     </Container>
   );
--- src/pages/CreateProducts/styledCreateProducts.js
+++ src/pages/CreateProducts/styledCreateProducts.js
@@ -26,11 +26,21 @@ export const Container = styled.div`
   }
 `;
 
-export const Form = styled.div`
+export const Form = styled.form`
   width: 100%;
   border-radius: 5px;
   background-color: #f2f2f2;
   padding: 20px;
+
+  .imageBlock {
+    display: flex;
+    justify-content: center;
+  }
+  .imageBlock img {
+    width: 150px;
+    height: 150px;
+    border-radius: 4px;
+  }
 
   input {
     width: 100%;
@@ -40,5 +50,26 @@ export const Form = styled.div`
     border: 1px solid #ccc;
     border-radius: 4px;
     box-sizing: border-box;
+  }
+
+  button {
+    width: 100%;
+    padding: 12px 20px;
+    margin: 8px 0;
+    display: flex;
+    justify-content: center;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    box-sizing: border-box;
+  }
+
+  button[type='submit'] {
+    cursor: pointer;
+    background: #4682b4;
+    color: #fff;
+  }
+
+  button[type='submit']:hover {
+    background: ${darken(0.03, '#4682b4')};
   }
 `;
